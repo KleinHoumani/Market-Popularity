9cc70cd4c2f9181cc2ac96501ab4116390b8fafa
KleinHoumani <84551085+KleinHoumani@users.noreply.github.com>
2022-10-09 00:14:02 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import json
 import requests
 import time
 import os
-from flask import Flask, jsonify, make_response
+from flask import Flask, make_response
 from threading import Thread
 
 client_id = os.environ.get('REDDIT_CLIENT')
@@ -21,17 +21,73 @@ headers = {
 
 subredditlist = ['wallstreetbets', 'stocks', 'shortsqueeze', 'investing', 'options', 'stockmarket', 'daytrading']
 time_list = ["day", 'week', 'month', 'year']
+sub_data = {}
 
-reddit_wallstreetbets_list_setup = ["setting up"]
-reddit_stocks_list_setup = ["setting up"]
 
-reddit_wallstreetbets_list = ["setting up"]
-reddit_stocks_list = ["setting up"]
-reddit_shortsqueeze_list = ["setting up"]
-reddit_investing_list = ["setting up"]
-reddit_options_list = ["setting up"]
-reddit_stockmarket_list = ["setting up"]
-reddit_daytrading_list = ["setting up"]
+class sub:
+    def __init__(self, sub_name):
+        self.sub_name = sub_name
+        self.data = ["setting up"]
+
+    def set_data(self):
+        times_combined = []
+        for timeframe in time_list:
+            symbolcount = []
+            subreddit = reddit.subreddit(self.sub_name)
+            hot_python = list(subreddit.top(timeframe, limit=100))
+            time.sleep(0.5)
+            with open('stocksymbols.txt', 'r') as symbolcheck:
+                for line in symbolcheck:
+                    x = 0
+                    finalsymbol1 = str(line.rstrip('\n'))
+                    finalsymbol2 = "$" + finalsymbol1
+                    for submission in hot_python:
+                        posttitle = submission.title
+                        titlesplit = posttitle.split()
+                        if finalsymbol1 in titlesplit or finalsymbol2 in titlesplit:
+                            x += 1
+
+                    if timeframe == "day":
+                        stock_range = "1d"
+                    if timeframe == "week":
+                        stock_range = "5d"
+                    if timeframe == "month":
+                        stock_range = "1mo"
+                    if timeframe == "year":
+                        stock_range = "1y"
+
+                    if x >= 1:
+                        try:
+                            yahoo_url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + finalsymbol1 + '?formatted=true&crumb=jEkX0k2sA5R&lang=en-US&region=US&events=div%7Csplit&includeAdjustedClose=true&interval=1d&range=' + stock_range + '&useYfid=true&corsDomain=finance.yahoo.com'
+                            data = requests.get(yahoo_url, headers=headers).json()
+                            most_recent_close = data['chart']['result'][0]['meta']['regularMarketPrice']
+                            old_close = data['chart']['result'][0]['meta']['chartPreviousClose']
+                            percent_change = (100 * most_recent_close / old_close) - 100
+                            data = {"stock": finalsymbol1, "postcount": str(x), "price": str(most_recent_close), "percentchange": str(round(percent_change, 2))}
+                            time.sleep(1)
+
+                            symbolcount.append(json.dumps(data) + ",")
+                        except:
+                            print('error ', finalsymbol1)
+            if len(symbolcount) >= 1:
+                symbolcountnew = symbolcount[:-1]
+                symbolcountnew.append(symbolcount[-1][:-1])
+
+                if timeframe != "year":
+                    times_combined.append('"' + timeframe + '": [' + "".join(symbolcountnew) + '],')
+                else:
+                    times_combined.append('"' + timeframe + '": [' + "".join(symbolcountnew) + ']')
+
+            self.data.clear()
+            self.data.append("{" + "".join(times_combined) + "}")
+
+
+def update_data():
+    for subreddit in subredditlist:
+        sub_data[subreddit] = sub(subreddit)
+        sub_data[subreddit].set_data()
+        time.sleep(1800)
+
 
 app = Flask(__name__)
 
@@ -42,137 +98,18 @@ def main_api():
         my_resp.headers['Access-Control-Allow-Origin'] = '*'
         return my_resp
 
-    @app.route("/reddit_wallstreetbets", methods=['GET'])
-    def reddit_wallstreetbets():
-        my_resp = make_response('{\n' + reddit_wallstreetbets_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
-
-    @app.route("/reddit_stocks", methods=['GET'])
-    def reddit_stocks():
-        my_resp = make_response('{\n' + reddit_stocks_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
-
-    @app.route("/reddit_shortsqueeze", methods=['GET'])
-    def reddit_shortsqueeze():
-        my_resp = make_response('{\n' + reddit_shortsqueeze_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
-
-    @app.route("/reddit_investing", methods=['GET'])
-    def reddit_investing():
-        my_resp = make_response('{\n' + reddit_investing_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
-
-    @app.route("/reddit_options", methods=['GET'])
-    def reddit_options():
-        my_resp = make_response('{\n' + reddit_options_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
-
-    @app.route("/reddit_stockmarket", methods=['GET'])
-    def reddit_stockmarket():
-        my_resp = make_response('{\n' + reddit_stockmarket_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
-
-    @app.route("/reddit_daytrading", methods=['GET'])
-    def reddit_daytrading():
-        my_resp = make_response('{\n' + reddit_daytrading_list[0] + '\n}')
-        my_resp.headers['Access-Control-Allow-Origin'] = '*'
-        return my_resp
+    @app.route("/reddit_<name>", methods=['GET'])
+    def subreddit_data(name):
+        for subreddit in subredditlist:
+            if name == subreddit:
+                resp = make_response(sub_data[subreddit].data[0])
+                resp.headers['Access-Control-Allow-Origin'] = '*'
+                return resp
+        return "No data found"
 
     if __name__ == "__main__":
         app.run()
 
-def set_data():
-    while True:
-        for sub_name in subredditlist:
-            times_combined = []
-            for timeframe in time_list:
-                symbolcount = []
-                x = 0
-                subreddit = reddit.subreddit(sub_name)
-                hot_python = list(subreddit.top(timeframe, limit=100))
-                time.sleep(0.5)
-                with open('stocksymbols.txt', 'r') as symbolcheck:
-                    for line in symbolcheck:
-                        finalsymbol1 = str(line.rstrip('\n'))
-                        finalsymbol2 = "$" + finalsymbol1
-                        for submission in hot_python:
-                            posttitle = submission.title
-                            titlesplit = posttitle.split()
-                            if finalsymbol1 in titlesplit:
-                                x += 1
-                            if finalsymbol2 in titlesplit:
-                                x += 1
-                        if timeframe == "day":
-                            stock_range = "1d"
-                        if timeframe == "week":
-                            stock_range = "5d"
-                        if timeframe == "month":
-                            stock_range = "1mo"
-                        if timeframe == "year":
-                            stock_range = "1y"
-                        if x >= 1:
-                            try:
-                                yahoo_url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + finalsymbol1 + '?formatted=true&crumb=jEkX0k2sA5R&lang=en-US&region=US&events=div%7Csplit&includeAdjustedClose=true&interval=1d&range=' + stock_range + '&useYfid=true&corsDomain=finance.yahoo.com'
-                                data = requests.get(yahoo_url, headers=headers).json()
-                                most_recent_close = data['chart']['result'][0]['meta']['regularMarketPrice']
-                                old_close = data['chart']['result'][0]['meta']['chartPreviousClose']
-                                percent_change = (100 * most_recent_close / old_close) - 100
-
-                                time.sleep(1)
-
-                                symbolcount.append('{\n' + '"stock": ' + '"' + str(finalsymbol1) + '",\n' + '"postcount": ' +
-                                               '"' + str(x) + '",\n' + '"price": ' + '"' + str(most_recent_close) +
-                                               '",\n' + '"percentchange": ' + '"' + str(round(percent_change, 2)) + '"' + '\n},')
-                            except:
-                                print('error', finalsymbol1)
-
-                            # symbolcount.append('{\n' + '"stock": ' + '"' + str(finalsymbol1) + '",\n' + '"postcount": ' +
-                            #                    '"' + str(x) + '"\n},')
-                        x -= x
-                try:
-                    symbolcountnew = symbolcount[:-1]
-                    symbolcountnew.append(symbolcount[-1][:-1])
-
-                    if timeframe != "year":
-                        times_combined.append('"' + timeframe + '": [' + "".join(symbolcountnew) + '],')
-                    else:
-                        times_combined.append('"' + timeframe + '": [' + "".join(symbolcountnew) + ']')
-                except:
-                    print("symbolcountnew fail")
-
-            if sub_name == "wallstreetbets":
-                reddit_wallstreetbets_list.clear()
-                reddit_wallstreetbets_list.append("".join(times_combined))
-            if sub_name == "stocks":
-                reddit_stocks_list.clear()
-                reddit_stocks_list.append("".join(times_combined))
-            if sub_name == "shortsqueeze":
-                reddit_shortsqueeze_list.clear()
-                reddit_shortsqueeze_list.append("".join(times_combined))
-            if sub_name == "investing":
-                reddit_investing_list.clear()
-                reddit_investing_list.append("".join(times_combined))
-            if sub_name == "options":
-                reddit_options_list.clear()
-                reddit_options_list.append("".join(times_combined))
-            if sub_name == "stockmarket":
-                reddit_stockmarket_list.clear()
-                reddit_stockmarket_list.append("".join(times_combined))
-            if sub_name == "daytrading":
-                reddit_daytrading_list.clear()
-                reddit_daytrading_list.append("".join(times_combined))
-        time.sleep(1800)
-
-def no_idling():
-    print('.')
-    time.sleep(120)
 
 Thread(target=main_api).start()
-Thread(target=set_data).start()
-Thread(target=no_idling).start()
+Thread(target=update_data).start()
