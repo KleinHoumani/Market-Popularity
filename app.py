f652e2f780338baa930db07ae81e97ca1712d91c
KleinHoumani <84551085+KleinHoumani@users.noreply.github.com>
2023-01-14 01:53:23 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -69,9 +69,7 @@ class sub:
                             symbol_count.append(data)
                         except:
                             print('error ', final_symbol1)
-                    self.data[timeframe] = sorted(symbol_count, key=itemgetter("postcount"), reverse=True)
-            print(self.data)
-        print(self.data)
+            self.data[timeframe] = sorted(symbol_count, key=itemgetter("postcount"), reverse=True)
 
 
 def update_data():
